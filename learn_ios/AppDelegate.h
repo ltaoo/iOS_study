c73d91713edb7293fdf8257e0454848fdbb782fd
ltaoo <ltaoo@ltaoodeMacBook-Pro.local>
2016-11-09 22:39:37 +0800

    Initial Commit

---
+++ learn_ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  learn_ios
+//
+//  Created by ltaoo on 2016/11/9.
+//  Copyright © 2016年 ltaoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
